b39a67a01a6546f7d3b9ca3c5fde32212eae77e0
dawoodkaleem <dawood9743@gmail.com>
2024-12-10 21:00:22 +0500

    Css updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,41 @@
-import { BrowserRouter } from "react-router";
-
+// import { BrowserRouter, Route, Routes } from "react-router";
+// import { Home } from "./pages/Home";
+// const App = () => {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// };
+// export default App;
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Service from "./pages/Service";
+import Navbar from "./components/Navbar";
 const App = () => {
-  return <h1>Hello the New React practics</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
+
 export default App;
