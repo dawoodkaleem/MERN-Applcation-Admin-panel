96eaca6f2c31ba4576d8a2a2387b5b15773cf1b8
dawoodkaleem <dawood9743@gmail.com>
2024-12-10 10:30:23 +0500

    Navigation created

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+// import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="logo-brand">
+            <a href="/">Dawood Technocl</a>
+            <nav className="">
+              <ul>
+                <li>
+                  <a href="/" />
+                  Home
+                </li>
+                <li>
+                  <a href="/" />
+                  Hom
+                </li>
+                <li>
+                  <a href="/" />
+                  Hom
+                </li>
+                <li>
+                  <a href="/" />
+                  Hom
+                </li>
+                <li>
+                  <a href="/" />
+                  Hom
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
