3d8073b331584ac1a221b053658768b682f53546
dawoodkaleem <dawood9743@gmail.com>
2024-12-10 21:01:52 +0500

    Form created login

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,83 @@
+// import React from "react";
+
+import { useState } from "react";
+
+const Login = () => {
+  const [user, SetUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    SetUser({
+      ...user,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <>
+      <section>
+        <main>
+          <div className="section-registratrion">
+            <div className="container grid grid-two-cols">
+              <div className="resgistration-image">
+                <img
+                  src="images/register.png"
+                  alt=""
+                  width="500"
+                  height="500"
+                />
+              </div>
+              {/* let tackeled registration form */}
+              <h1 className="main-heading mb-3">Login now </h1>
+              <br />
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="email">email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="enter your email"
+                    id="email"
+                    required
+                    autoComplete="off"
+                    value={user.email}
+                    onChange={handleInput}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="password">password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="enter your password"
+                    id="password"
+                    required
+                    autoComplete="off"
+                    value={user.password}
+                    onChange={handleInput}
+                  />
+                </div>
+                <br />
+                <button type="submit" className="btn btn-submit">
+                  Login Now
+                </button>
+              </form>
+            </div>
+          </div>
+        </main>
+      </section>{" "}
+    </>
+  );
+};
+
+export default Login;
