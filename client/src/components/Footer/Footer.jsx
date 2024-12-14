28312e28a5e532ff61de9e88ee6c362004d39216
dawoodkaleem <dawood9743@gmail.com>
2024-12-14 18:07:22 +0500

    Footer and Footer Css Created

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+// import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <p>@dawoodtechnical 2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
