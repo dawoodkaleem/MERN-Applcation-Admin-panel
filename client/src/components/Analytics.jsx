8a55dfcd92f31b8ed43f1f7e8162c40da3e28f46
dawoodkaleem <dawood9743@gmail.com>
2024-12-14 16:24:46 +0500

    Updated css

---
+++ client/src/components/Analytics.jsx
@@ -0,0 +1,28 @@
+// import React from "react";
+
+function Analytics() {
+  return (
+    <section className="section-analytics">
+      <div className="container grid grid-four-cols">
+        <div className="div1">
+          <h2>50+</h2>
+          <p>Company registers </p>
+        </div>
+        <div className="div1">
+          <h2>150+</h2>
+          <p>Project Done</p>
+        </div>
+        <div className="div1">
+          <h2>250+</h2>
+          <p>Happy Done</p>
+        </div>
+        <div className="div1">
+          <h2>650K+</h2>
+          <p>youtube subscriers</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Analytics;
