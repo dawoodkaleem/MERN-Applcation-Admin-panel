4816b551b9a4a4dd94baad3eed0bec9226555c9b
dawoodkaleem <dawood9743@gmail.com>
2024-12-18 10:43:06 +0500

    Fetched service data from database

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,9 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState("");
   const storeTokenInLS = (serverToken) => {
     return localStorage.setItem("token", serverToken);
   };
@@ -17,12 +18,34 @@ export const AuthProvider = ({ children }) => {
   };
   // JWT Authtication to get the Currently User loggedin data
 
+  const useAuthentication = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/user", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("user data", data.userData);
+        setUser(data.userData);
+      }
+    } catch (error) {
+      console.error("Error fetching user data ", error);
+    }
+  };
+  useEffect(() => {
+    useAuthentication();
+  }, []);
+
   AuthProvider.propTypes = {
     children: PropTypes.node.isRequired, // Ensure children is a valid React node
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, storeTokenInLS, LogoutUser }}>
+    <AuthContext.Provider
+      value={{ isLoggedIn, storeTokenInLS, LogoutUser, user }}>
       {children}
     </AuthContext.Provider>
   );
