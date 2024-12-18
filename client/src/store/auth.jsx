215e631c38b707ebda8579fb83c8c11181ad10d8
dawoodkaleem <dawood9743@gmail.com>
2024-12-18 11:35:15 +0500

    Service card added

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -5,6 +5,7 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState("");
+  const [services, setServices] = useState("");
   const storeTokenInLS = (serverToken) => {
     return localStorage.setItem("token", serverToken);
   };
@@ -35,7 +36,23 @@ export const AuthProvider = ({ children }) => {
       console.error("Error fetching user data ", error);
     }
   };
+  // to fetch the services data from the database
+  const getServices = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/data/service", {
+        method: "GET",
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Data for services", data.msg);
+        setServices(data.msg);
+      }
+    } catch (error) {
+      console.log(`services frontend error: ${error}`);
+    }
+  };
   useEffect(() => {
+    getServices();
     useAuthentication();
   }, []);
 
@@ -45,7 +62,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn, storeTokenInLS, LogoutUser, user }}>
+      value={{ isLoggedIn, storeTokenInLS, LogoutUser, user, services }}>
       {children}
     </AuthContext.Provider>
   );
