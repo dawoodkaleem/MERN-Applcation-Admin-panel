96a79b2f9c42fe7155e03a213ed60111b2dfc6af
dawoodkaleem <dawood9743@gmail.com>
2024-12-29 15:00:34 +0500

    getContactsData function in admin contact panel

--- client/src/pages/Admin-Contacts.jsx
+++ client/src/pages/Admin-Contacts.jsx
@@ -1,3 +1,27 @@
+import { useEffect } from "react";
+import { useAuth } from "../store/auth";
 export const AdminContacts = () => {
-  return <h1>Admin Contact panel</h1>;
+  const { authorizationToken } = useAuth();
+
+  const getContactsData = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/admin/contacts`, {
+        method: "GET",
+        headers: {
+          Authorization: authorizationToken,
+        },
+      });
+      const data = await response.json();
+      console.log("contacts Data", data);
+      if (response.ok) {
+        console.log("contact data", response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getContactsData();
+  });
+  return <h1>Hello thapaContacts Contact panel</h1>;
 };
